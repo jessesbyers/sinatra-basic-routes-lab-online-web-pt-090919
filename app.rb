96f8ac8bc415a14e4ade8b16f520ecdf4a2c06ce
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 14:08:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,16 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    "Hello, World!"
+# the favorite-song route should display "My favorite song is __".
+get '/' do
+  "Hello, World!"
+end
+
+  get '/name' do
+    "My name is __"
   end
 
-  get '/' do
-    "Hello, World!"
+  get '/hometown' do
+    "My hometown is __"
   end
 
-  get '/' do
-    "Hello, World!"
+  get '/favorite-song' do
+    "My favorite song is __"
   end
 
 end
